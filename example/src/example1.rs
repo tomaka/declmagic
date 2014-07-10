43bff2715bcba4b477ea0f6e2eb9662deb656f07
Tomaka17 <pierre.krieger1708@gmail.com>
2014-07-10 12:13:58 +0200

    Rework a bit the example

--- example/src/example1.rs
+++ example/src/example1.rs
@@ -3,7 +3,7 @@ extern crate declmagic;
 use declmagic::resources::dir_loader::DirLoader;
 
 fn main() {
-	let loader = DirLoader::new(Path::new("resources"));
+	let loader = DirLoader::new(::std::os::self_exe_path().unwrap().join("resources"));
 
 	declmagic::exec_game(loader);
 }
